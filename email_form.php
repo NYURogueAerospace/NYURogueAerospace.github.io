db9dc357570c37c0ba083e83d24b582522ef5315
dwu101 <dan.q.w100@gmail.com>
2022-11-19 21:23:22 -0500

    aligned prof pics

--- email_form.php
+++ email_form.php
@@ -11,4 +11,4 @@ $message = "Sent";
 echo "<script type='text/javascript'>alert('$message');</script>";
  window.open("http://google.com");
 //echo "<meta http-equiv='refresh' content='0'>";
-?>
+?> 
